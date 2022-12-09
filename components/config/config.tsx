615bb3da1fe7aefd47c5823fd492a66919d132f4
Teruki Tada <tadateruki.public@gmail.com>
2022-12-09 19:52:02 +0900

    [fix] ページのサムネイルを変更

--- components/config/config.tsx
+++ components/config/config.tsx
@@ -4,7 +4,7 @@ const Config = (props: { title: string, subtitle: string }) => {
   return(
     <Head>
       <title>{props.title+" - Peruki's Portfolio"}</title>
-      <meta property="og:image" content="static/myicon.jpg" />
+      <meta property="og:image" content={process.env.NEXT_PUBLIC_PORTFOLIO_BASE_URL+"/static/welcome.jpg"} />
       <meta property="og:description" content={props.subtitle}/>
     </Head>
   )
