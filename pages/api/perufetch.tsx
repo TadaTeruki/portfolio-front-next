9ec5350a138ee35f46746871d974e506bdad6fb6
Teruki Tada <tadateruki.public@gmail.com>
2023-05-28 16:26:32 +0900

    [add] perufetchのrouteを追加

---
+++ pages/api/perufetch.tsx
@@ -0,0 +1,10 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import RequestPerufetch from '../../packages/requests/Perufetch';
+
+export default async function handler(
+    _: NextApiRequest,
+    res: NextApiResponse<string>
+) {
+    const perufetch = await RequestPerufetch();
+    res.status(200).send(perufetch);
+}
